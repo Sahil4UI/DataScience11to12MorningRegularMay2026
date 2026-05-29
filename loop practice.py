f09665725e88c5813eee2dbc29f9d805fc776fd0
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-05-29 11:52:27 +0530

    Create loop practice.py

---
+++ loop practice.py
@@ -0,0 +1,19 @@
+#Q1
+'''
+for i in range(1,11):
+    print(f"2X{i}={2*i}")
+'''
+#Q2
+'''
+result = 0
+for i in range(1,11):
+    result+=i
+print(result)
+'''
+#Q3
+result = 1
+x = 5
+for i in range(1,x+1):
+    result*=i
+    
+print(result)
